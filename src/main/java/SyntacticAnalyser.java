7fb0e204b8229ac0b8945d2f82afe882f9c8289b
Mauricio Concatto <mauriciocc@outlook.com>
2014-08-19 20:51:01 -0300

    Commit aula

--- src/main/java/SyntacticAnalyser.java
+++ src/main/java/SyntacticAnalyser.java
@@ -1,74 +1,97 @@
+
 import java.util.*;
 
-public class SyntacticAnalyser
-{
+public class SyntacticAnalyser {
+
     private List<Token> tokens;
     private int level = 0;
-    
-    public SyntacticAnalyser() {}
 
-    private void call(String s) { 
-        for (int i = 0; i < level; i++) { 
-            System.out.print("  "); 
+    public SyntacticAnalyser() {
+    }
+
+    private void call(String s) {
+        for (int i = 0; i < level; i++) {
+            System.out.print("  ");
         }
         System.out.println(s);
     }
 
-    public void analyse(List<Token> tokens)
-    {
-        this.tokens = tokens;
-        tokens.add(new Token('$'));
-        S();
-        System.out.println("remaining tokens: " + tokens);
+    public double analyse(List<Token> tokens) {
+        try {
+            this.tokens = tokens;
+            tokens.add(new Token('$'));
+            return S();
+        } finally {
+            System.out.println("remaining tokens: " + tokens);
+        }
     }
-    
-    private void S()
-    {
+
+    private double S() {
         call("S");
-        E();
+        return E();
     }
-    
-    private void E() // E -> T | T + E
+
+    private double E() // E -> T | T + E
     {
-        level++; call("E");
-        T();
+        double value = 0;
+        level++;
+        call("E");
+        value = T();
         if (tokens.get(0).getType() == '+') {
-            tokens.remove(0); //System.out.print("+ ");
-            E();
+            tokens.remove(0);
+            value += E();
+        } else if (tokens.get(0).getType() == '-') {
+            tokens.remove(0);
+            value -= E();
         }
         level--;
+        return value;
     }
-    
-    private void T() // T -> F | F * T
+
+    private double T() // T -> F | F * T
     {
-        level++; call("T");
-        F();
+        double value = 0;
+        level++;
+        call("T");
+        value = F();
         if (tokens.get(0).getType() == '*') {
-            tokens.remove(0); //System.out.print("* ");
-            T();
-        }
-        level--;
-    }
-    
-    private void F() // F -> n | (E)
-    {
-        level++; call("F");
-        if (tokens.get(0).getType() == 'n') {
-            tokens.remove(0); //System.out.print("n ");
-        }
-        else if (tokens.get(0).getType() == '(') {
-            tokens.remove(0); //System.out.print("( ");
-            E();
-            if (tokens.get(0).getType() != ')') {
-                System.out.println("error: unexpected symbol " + tokens.get(0).getType());
-                System.exit(0);
+            tokens.remove(0);
+            value *= T();
+        } else if (tokens.get(0).getType() == '/') {
+            tokens.remove(0);
+            double t = T();
+            if(t == 0) {
+                throw new ArithmeticException("cannot divide by 0");
             }
-            tokens.remove(0); //System.out.print(") ");
-        }
-        else {
-            System.out.println("error: unexpected symbol " + tokens.get(0).getType());
-            System.exit(0);
+            value /= t;
         }
         level--;
+        return value;
+    }
+
+    private double F() // F -> n | (E)
+    {
+        double value = 0;
+        level++;
+        call("F");
+        if (tokens.get(0).getType() == 'n') {
+            value = tokens.remove(0).getValue();
+        } else if (tokens.get(0).getType() == '(') {
+            tokens.remove(0);
+            value = E();
+            if (tokens.get(0).getType() != ')') {
+                error(tokens.get(0));
+            }
+            tokens.remove(0);
+        } else {
+            error(tokens.get(0));
+        }
+        level--;
+        return value;
+    }
+
+    private void error(Token t) {
+        System.out.println("error: unexpected symbol " + t.getType());
+        throw new RuntimeException();
     }
 }
--- src/main/java/TestSyntacticAnalyser.java
+++ src/main/java/TestSyntacticAnalyser.java
@@ -1,17 +1,18 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class TestSyntacticAnalyser
 {
     public static void main(String[] args)
     {
-        String s = " 1 + 2.0* ( 03 *4.56 + 78.9)";
+        //String s = " 1 + 2.0* ( 03 *4.56 + 78.9)";
+        //String s = " (1 + 2) * 3";
+        String s = "12 + 50 - 15 * 4/2";
         System.out.println("Entrada: '" + s + "'");
         
         LexicalAnalyser la = new LexicalAnalyser(s);
         List<Token> tokens = la.analyse();
         
         SyntacticAnalyser sa = new SyntacticAnalyser();
-        sa.analyse(tokens);
+        System.out.println(sa.analyse(tokens));
     }
 }
