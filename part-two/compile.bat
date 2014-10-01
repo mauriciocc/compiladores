a5b56943d3ad827948d279bb1d63a2e006b9de45
Mauricio Colognese Concatto <mauriciocc@outlook.com>
2014-09-30 22:26:58 -0300

    Commit aula

--- part-two/compile.bat
+++ part-two/compile.bat
@@ -3,6 +3,7 @@ rm CafeLexer*
 rm CafeParser*
 rm *.class
 rm *.j
+javac Runtime.java
 java -jar antlr-3.2.jar Cafe.g
 javac -cp antlr-3.2.jar *.java
 java -cp .;antlr-3.2.jar CafeParser < %1 > Teste.j
